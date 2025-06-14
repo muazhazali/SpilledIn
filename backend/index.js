665ea339a815350157bf2daed44576a6655b0a47
qlfzn <aqilfauzan09@gmail.com>
2025-06-15 02:43:20 +0800

    add confession route

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,16 @@ const app = express();
 const port = 5050;
 
 const userRoutes = require('./routes/userRoute');
+const confessionRoutes = require('./routes/confessionRoutes');
+const confessionFeedRoutes = require('./routes/confessionFeedRoutes');
 
 app.get('/', (req, res) => {
   res.send("Hello world");
 })
 
 app.use('/api/users/', userRoutes);
+app.use('/api/confession/', confessionRoutes);
+app.use('/api/feed/', confessionFeedRoutes);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
